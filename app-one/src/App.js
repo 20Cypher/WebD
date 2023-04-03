98eda7396d0130f3d748a11e81b3af1407929659
chirag1020 <chiragcg2003@gmail.com>
2023-04-04 04:19:44 +0530

    day2

--- app-one/src/App.js
+++ app-one/src/App.js
@@ -1,17 +1,32 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import About from "./components/About";
+import React, { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
+
   return (
     <>
-    <div className="App">
-      <Navbar title="blagh"/>
-      <h1>Welcome!</h1>
-      <div className="container my-3">
-        <TextForm heading="Enter text to analyse"/>
+      <div className="App">
+        <Navbar title="blagh" mode={mode} toggleMode={toggleMode} />
+        <h1>Welcome!</h1>
+        <div className="container my-3">
+          <TextForm heading="Enter text to analyse" mode={mode}/>
+        </div>
+        <About />
       </div>
-    </div>
     </>
   );
 }
--- app-one/src/components/TextForm.js
+++ app-one/src/components/TextForm.js
@@ -20,9 +20,36 @@ export default function TextForm(props) {
     setText(newText);
   };
 
+  const speak = () => {
+    let msg = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(msg);
+    const toogle = document.getElementById("toggle");
+    if (toogle.textContent === "Speak") {
+      toogle.innerHTML = "Stop";
+    } else {
+      toogle.innerHTML = "Speak";
+      if ((toogle.innerHTML = "Speak")) {
+        window.speechSynthesis.cancel();
+      }
+    }
+  };
+
+  const downloadTxtFile = () => {
+    const element = document.createElement("a");
+    const file = new Blob([text], {
+      type: "text/plain",
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = "myFile.txt";
+    element.click();
+  };
+
   return (
     <>
-      <div>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
         <div className="mb-3">
           <h1>{props.heading}</h1>
           <textarea
@@ -31,6 +58,10 @@ export default function TextForm(props) {
             rows="8"
             value={text}
             onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
           ></textarea>
         </div>
         <button className="btn btn-primary" onClick={handleUpper}>
@@ -41,8 +72,22 @@ export default function TextForm(props) {
           {" "}
           Convert to LowerCase{" "}
         </button>
+        <button
+          type="submit"
+          onClick={speak}
+          className="btn btn-warning"
+          id="toggle"
+        >
+          Speak
+        </button>
+        <button className="btn btn-danger mx-5" onClick={downloadTxtFile}>
+          Download Text
+        </button>
       </div>
-      <div className="container my-4">
+      <div
+        className="container my-4"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
         <h2>Your Text Summary: </h2>
         <p>
           {text.split(" ").length} words and {text.length} characters
