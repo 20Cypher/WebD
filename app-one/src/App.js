e3e3cbbdbc5f46945c2978225f8bc8bff19ca34d
chirag1020 <chiragcg2003@gmail.com>
2023-04-09 04:16:10 +0530

    color modes

--- app-one/src/App.js
+++ app-one/src/App.js
@@ -4,9 +4,10 @@ import TextForm from "./components/TextForm";
 import About from "./components/About";
 import Alert from "./components/Alert";
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
 
 function App() {
+
   const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
@@ -25,27 +26,44 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
-    } else {
+    } else if (mode === "dark") {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
-    }
+    } 
   };
+
+  const modeRed = () => {
+    // setMode('red');
+    document.body.style.backgroundColor = 'red';
+  }
+
+  const modeYellow = () => {
+    // setMode('red');
+    document.body.style.backgroundColor = 'yellow';
+  }
+
+  const modeGreen = () => {
+    // setMode('red');
+    document.body.style.backgroundColor = 'green';
+  }
 
   return (
     <>
-      <div className="App">
-        <Navbar title="blagh" mode={mode} toggleMode={toggleMode} />
+      <Router>
+        <Navbar title="blagh" mode={mode} toggleMode={toggleMode} modeRed={modeRed} modeGreen={modeGreen} modeYellow={modeYellow}/>
         <Alert alert={alert} />
         <div className="container my-3">
-          <TextForm
-            showAlert={showAlert}
-            heading="Enter text to analyse"
-            mode={mode}
-          />
+          <Routes>
+            <Route path="/about" element={<About/>} />
+            <Route path="/" element={<TextForm
+                showAlert={showAlert}
+                heading="Enter text to analyse"
+                mode={mode}
+              />} />
+          </Routes>
         </div>
-        <About />
-      </div>
+      </Router>
     </>
   );
 }
--- app-one/src/components/Navbar.js
+++ app-one/src/components/Navbar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -6,9 +8,9 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to="/">
           Text Utils - {props.title}
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -23,34 +25,31 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <Link className="nav-link" to="/about">
                 About
-              </a>
+              </Link>
             </li>
           </ul>
+
           <div
-            className={`form-check form-switch text-${
-              props.mode === "light" ? "dark" : "light"
-            }`}
+            className="btn-group mx-3"
+            role="group"
+            aria-label="Basic mixed styles example"
           >
-            <input
-              className="form-check-input"
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckDefault"
-              onClick={props.toggleMode}
-            />
-            <label
-              className="form-check-label"
-              htmlFor="flexSwitchCheckDefault"
-            >
-              Enable Red Mode
-            </label>
+            <button type="button" className="btn btn-danger" onClick={props.modeRed}>
+              Red
+            </button>
+            <button type="button" className="btn btn-warning" onClick={props.modeYellow}>
+              Yellow
+            </button>
+            <button type="button" className="btn btn-success" onClick={props.modeGreen}>
+              Green
+            </button>
           </div>
 
           <div
