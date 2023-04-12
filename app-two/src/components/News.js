c618bcc5be8557c620d71cd40a16dc9a0469e987
chirag1020 <chiragcg2003@gmail.com>
2023-04-13 03:37:34 +0530

    added categories

--- app-two/src/components/News.js
+++ app-two/src/components/News.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./spinner";
+import PropTypes from "prop-types";
 
 export class News extends Component {
+  static defaultPorps = {
+    country: "in",
+    pageSize: 9,
+    category: "general",
+  };
+
+  static porpsTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
   constructor() {
     super();
     this.state = {
@@ -12,36 +26,55 @@ export class News extends Component {
   }
 
   async componentDidMount() {
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=783068a340484681b296b8353d4669cb&page=1&pageSize=20";
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=783068a340484681b296b8353d4669cb&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
+    this.setState({ loading: true });
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loading: false,
     });
   }
 
   handlePrev = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=783068a340484681b296b8353d4669cb&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${
+      this.props.category
+    }&apiKey=783068a340484681b296b8353d4669cb&page=${
       this.state.page - 1
-    }&pageSize=20`;
+    }&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
+    this.setState({ loading: true });
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles, page: this.state.page - 1 });
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page - 1,
+      loading: false,
+    });
   };
 
   handleNext = async () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    if (
+      this.state.page + 1 >
+      Math.ceil(this.state.totalResults / this.props.pageSize)
+    ) {
     } else {
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=783068a340484681b296b8353d4669cb&page=${
+      let url = `https://newsapi.org/v2/top-headlines?country=${
+        this.props.country
+      }&category=${
+        this.props.category
+      }&apiKey=783068a340484681b296b8353d4669cb&page=${
         this.state.page + 1
-      }&pageSize=20`;
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
       let data = await fetch(url);
       let parsedData = await data.json();
       this.setState({
         articles: parsedData.articles,
         page: this.state.page + 1,
+        loading: false,
       });
     }
   };
@@ -49,24 +82,26 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h2>Top Headlines</h2>
+        <h1 className="text-center my-4">News - Top Headlines</h1>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((e) => {
-            return (
-              <div className="col-md-4" key={e.url}>
-                <NewsItem
-                  title={e.title ? e.title.slice(0, 45) : ""}
-                  desc={e.description ? e.description.slice(0, 88) : ""}
-                  imgurl={
-                    !e.urlToImage
-                      ? "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg"
-                      : e.urlToImage
-                  }
-                  newsurl={e.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((e) => {
+              return (
+                <div className="col-md-4" key={e.url}>
+                  <NewsItem
+                    title={e.title ? e.title.slice(0, 45) : ""}
+                    desc={e.description ? e.description.slice(0, 88) : ""}
+                    imgurl={
+                      !e.urlToImage
+                        ? "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg"
+                        : e.urlToImage
+                    }
+                    newsurl={e.url}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
@@ -81,6 +116,10 @@ export class News extends Component {
             type="button"
             className="btn btn-primary"
             onClick={this.handleNext}
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
           >
             Next &rarr;
           </button>
